6438287bfb7e0e91c16f761ec351da7fb5658a05
GaboJohn <johngabotsalelwe@gmail.com>
2024-01-13 12:44:04 -0800

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize the BaseModel instance.
 
@@ -18,9 +18,23 @@ class BaseModel:
             created_at (datetime): Timestamp of the instance creation.
             updated_at (datetime): Timestamp of the instance's last update.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                 if key != '__class__':
+                     if key in ['created_at', 'updated_at']:
+                         setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                     else:
+                         setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """
