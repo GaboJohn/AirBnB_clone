8319a30d9038d13cf19b4d94e416222047cf57e7
GaboJohn <johngabotsalelwe@gmail.com>
2024-01-16 00:46:54 -0800

    All instances by class name

--- console.py
+++ console.py
@@ -11,14 +11,15 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
+
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
-    def do_quit(self, mint):
+    def do_quit(self, args):
         """Exit the command interpreter."""
         return True
 
-    def do_EOF(self, mint):
+    def do_EOF(self, args):
         """Exit the command interpreter (Ctrl-D)."""
         print()
         return True
@@ -27,15 +28,15 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing on an empty line."""
         pass
 
-    def do_create(self, mint):
+    def do_create(self, args):
         """
         Creates a new instance of BaseModel, saves it, and prints the id
         """
-        if not mint:
+        if not args:
             print("** class name missing **")
             return
         try:
-            inst = eval(mint)()
+            inst = eval(args)()
             inst.save()
             print(inst.id)
         except NameError:
@@ -50,7 +51,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return
 
-        arg_list = args.spilt()
+        arg_list = args.split()
         class_name = arg_list[0]
         try:
             eval(class_name)
@@ -62,13 +63,13 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
             return
 
-            obj_id = arg_list[1]
-            key = "{}.{}".format(class_name, obj_id)
-            obj = storage.all().get(key)
-            if obj:
-                print(obj)
-            else:
-                print("** no instance found **")
+        obj_id = arg_list[1]
+        key = "{}.{}".format(class_name, obj_id)
+        obj = storage.all().get(key)
+        if obj:
+            print(obj)
+        else:
+            print("** no instance found **")
 
     def do_destroy(self, args):
         """
@@ -80,16 +81,16 @@ class HBNBCommand(cmd.Cmd):
             return
 
         arg_list = args.split()
-        class_name = args_list[0]
+        class_name = arg_list[0]
         try:
             eval(class_name)
         except NameError:
-             print("** class doesn't exist **")
-             return
-        
+            print("** class doesn't exist **")
+            return
+
         if len(arg_list) < 2:
-             print("** instance id missing **")
-             return
+            print("** instance id missing **")
+            return
 
         obj_id = arg_list[1]
         key = "{}.{}".format(class_name, obj_id)
@@ -104,13 +105,14 @@ class HBNBCommand(cmd.Cmd):
         """
         Updates an instance based on the class name and id by adding or updating attribute.
         Usage: update <class name> <id> <attribute name> "<attribute value>"
+               update <class name> <id> <dictionary representation>
         """
         if not args:
             print("** class name missing **")
             return
 
-        args_list = args.split()
-        class_name = args_list[0]
+        arg_list = args.split()
+        class_name = arg_list[0]
         try:
             eval(class_name)
         except NameError:
@@ -121,47 +123,75 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
             return
 
-            obj_id = args[1]
-            key = "{}.{}".format(class_name, obj_id)
-            obj = storage.all().get(key)
-            if obj:
-                if len(args) < 3:
-                    print("** attribute name missing **")
-                    return
+        obj_id = arg_list[1]
+        key = "{}.{}".format(class_name, obj_id)
+        obj = storage.all().get(key)
+        if obj:
+            if len(arg_list) < 3:
+                print("** attribute name missing **")
+                return
 
-                attr_name = args[2]
-                if len(args) < 4:
-                    print("** value missing **")
+            attr_name = arg_list[2]
+            if len(arg_list) < 4:
+                print("** value missing **")
+                return
+
+            attr_value = arg_list[3].strip('"')
+            if attr_value.startswith("{") and attr_value.endswith("}"):
+                try:
+                    attr_dict = eval(attr_value)
+                    for k, v in attr_dict.items():
+                        setattr(obj, k, v)
+                except (NameError, SyntaxError):
+                    print("** invalid dictionary representation **")
                     return
-                attr_value = args[3].strip('"')
+            else:
                 if hasattr(obj, attr_name):
                     attr_type = type(getattr(obj, attr_name))
                     setattr(obj, attr_name, attr_type(attr_value))
-                    storage.save()
                 else:
                     print("** attribute doesn't exist **")
-            else:
-                print("** no instance found **")
+                    return
+
+            storage.save()
+        else:
+            print("** no instance found **")
 
     def do_all(self, args):
         """
         Prints all string representation of all instances based or not on the class name.
         Usage: all [<class name>]
+               <class name>.all()
         """
+        try:
+            arg_list = args.split()
+            objects = storage.all()
 
-        args_list = args.split()
-        objects = storage.all()
-        
-        if not arg_list:
-            print([str(obj) for obj in objects.values()])
-        else:
-            try:
+            if not arg_list:
+                print([str(obj) for obj in objects.values()])
+            else:
                 class_name = arg_list[0]
-                eval(class_name)
-                filt_obj = [str(obj) for obj in objects.values() if obj.__class__.__name__ == class_name]
-                print(filt_obj)
-            except NameError:
-                print("** class doesn't exist **")
+                try:
+                    obj_class = eval(class_name)
+                    if len(arg_list) > 1 and arg_list[1] == '.all()':
+                        obj_list = obj_class.all()
+                    else:
+                        raise AttributeError(f"Class '{class_name}' has no attribute 'all'")
+                    print([str(obj) for obj in obj_list])
+                except (NameError, AttributeError) as e:
+                    print(f"** {e} **")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def default(self, line):
+        """Called on an input line when the command prefix is not recognized."""
+        try:
+            # Execute the input line as Python code
+            eval_result = eval(line)
+            if eval_result is not None:
+                print(eval_result)
+        except Exception as e:
+            print(f"Error: {e}")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """Defines the FileStorage class."""
 
 import json
+import datetime
 
 class FileStorage:
     """Serializes instances to a JSON file and deserializes JSON file to instances."""
@@ -15,7 +16,7 @@ class FileStorage:
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
         if obj:
-            key = "{}.{}".format(type(obj).__class__.__name__, obj.id)
+            key = "{}.{}".format(type(obj).__name__, obj.id)
             self.__objects[key] = obj
 
     def save(self):
@@ -27,6 +28,67 @@ class FileStorage:
         with open(self.__file_path, 'w', encoding='utf-8') as file:
             json.dump(new_dict, file)
 
+    def get_class_instances(self, class_name):
+        """Returns a list of instances of the specified class."""
+        instances = []
+        for key, value in self.__objects.items():
+             if class_name in key:
+                 instances.append(value)
+        return instances
+
+    def classes(self):
+        """Returns dictionary of valid classes and their references."""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.city import City
+        from models.state import State
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes_dict = {
+            "BaseModel": {
+                "id": str,
+                "created_at": datetime.datetime,
+                "updated_at": datetime.datetime
+            },
+            "User": {
+                "email": str,
+                "password": str,
+                "first_name": str,
+                "last_name": str
+            },
+            "Place": {
+                "city_id": str,
+                "name": str,
+                "user_id": str,
+                "description": str,
+                "number_rooms": int,
+                "number_bathrooms": int,
+                "max_guest": int,
+                "price_by_night": int,
+                "latitude": float,
+                "longitude": float,
+                "amenity_ids": list
+            },
+            "City": {
+                "state_id": str,
+                "name": str
+            },
+            "State": {
+                "name": str
+            },
+            "Amenity": {
+                "name": str
+            },
+            "Review": {
+                "place_id": str,
+                "user_id": str,
+                "text": str
+            }
+        }
+        return classes_dict
+
     def reload(self):
         """Deserializes the JSON file to __objects."""
         try:
@@ -34,8 +96,14 @@ class FileStorage:
                 loaded_objs = json.load(file)
                 for key, obj_dict in loaded_objs.items():
                     class_name, obj_id = key.split('.')
-                    obj_class = eval(class_name)
-                    obj_instance = obj_class(**obj_dict)
-                    self.__objects[key] = obj_instance
+                    try:
+                        obj_class = self.classes().get(class_name)
+                        if obj_class:
+                            obj_instance = obj_class(**obj_dict)
+                            self.__objects[key] = obj_instance
+                        else:
+                            print(f"Class '{class_name}' not found")
+                    except Exception as e:
+                        print(f"Error creating instance: {e}")
         except FileNotFoundError:
-            pass                                             
+            pass                            
